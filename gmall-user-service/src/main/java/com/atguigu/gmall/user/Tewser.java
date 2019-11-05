8896dd841af56d464808280b8c23c77e495736cf
BSQ <634269333@qq.com>
2019-11-05 22:23:41 +0800

    fastdfs客户端相关

---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/Tewser.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.user;
+
+/**
+ * @auther Bao
+ * @date 2019/9/26 17:36
+ */
+public class Tewser {
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+       // thread.join();
+    }
+}
